ff65df943af26f9ba1025cb8f5452d184115eb5c
brawal <birendra.rawal@gmail.com>
2016-09-14 18:33:41 -0400

    This is the first project to commit in the github

---
+++ SeleniumWithJenkins/src/com/test/selenium/jenkins/TestSeleniumWithJenkins.java
@@ -0,0 +1,14 @@
+package com.test.selenium.jenkins;
+
+//import org.junit.Test;
+import org.testng.annotations.Test;
+
+
+public class TestSeleniumWithJenkins {
+
+	@Test
+	public void testSelwithJenkins(){
+		System.out.println("Hello World!, Test the Selenium jave file from the Jenkins");
+	}
+	
+}
